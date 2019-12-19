f257ff14552e3d9ff9d8ee4565f23977c0e665c9
Stephane Lee <stayfan@google.com>
2019-12-19 14:57:04 -0800

    b1c1: health HAL 2.1
    Bug: b/137790244
    Test: lshal debug (health service)
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: Id15d203bd791d0867972da98f6998610723b9332
    Signed-off-by: Kanishk <kanishkthederp@gmail.com>

--- device.mk
+++ device.mk
@@ -354,7 +354,8 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service.crosshatch
+    android.hardware.health@2.1-impl-crosshatch \
+    android.hardware.health@2.1-service
 
 # Storage health HAL
 PRODUCT_PACKAGES += \
