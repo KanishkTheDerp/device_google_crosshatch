bc088ad7c3e563c1882bb0af07667d51ecc36ac6
KanishkTheDerp <kanishkthederp@gmail.com>
2022-08-08 19:16:16 +0530

    b1c1: Initial VoltageOS changes
    Signed-off-by: KanishkTheDerp <kanishkthederp@gmail.com>

--- voltage_blueline.mk
+++ voltage_blueline.mk
@@ -4,28 +4,27 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
 
--include device/google/crosshatch/device-arrow.mk
+-include device/google/crosshatch/device-voltage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := arrow_blueline
+PRODUCT_NAME := voltage_blueline
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3
 TARGET_MANUFACTURER := Google
-
-# ArrowOS additions
-DEVICE_MAINTAINER := ReallySnow
-$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=blueline \
     PRIVATE_BUILD_DESC="blueline-user 12 SP1A.210812.016.C1 8029091 release-keys"
 
 BUILD_FINGERPRINT := google/blueline/blueline:12/SP1A.210812.016.C1/8029091:user/release-keys
+
+#VoltageOS build type
+VOLTAGE_BUILD_TYPE = OFFICIAL
 
 $(call inherit-product, vendor/google/blueline/blueline-vendor.mk)
--- voltage_crosshatch.mk
+++ voltage_crosshatch.mk
@@ -4,28 +4,27 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
 
--include device/google/crosshatch/device-arrow.mk
+-include device/google/crosshatch/device-voltage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := arrow_crosshatch
+PRODUCT_NAME := voltage_crosshatch
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3 XL
 TARGET_MANUFACTURER := Google
-
-# ArrowOS additions
-DEVICE_MAINTAINER := ReallySnow
-$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=crosshatch \
     PRIVATE_BUILD_DESC="crosshatch-user 12 SP1A.210812.016.C1 8029091 release-keys"
 
 BUILD_FINGERPRINT := google/crosshatch/crosshatch:12/SP1A.210812.016.C1/8029091:user/release-keys
+
+#VoltageOS build type
+VOLTAGE_BUILD_TYPE = OFFICIAL
 
 $(call inherit-product, vendor/google/crosshatch/crosshatch-vendor.mk)
