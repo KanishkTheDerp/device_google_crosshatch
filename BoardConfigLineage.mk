5a630b2d48787e13904786dfc8cb0614cb606203
Kanishk <kanishkthederp@gmail.com>
2024-04-30 12:29:36 +0530

    b1c1: drop vendor/dfm
    • VoltageOS doesn't use device_framework_matrix.xml in it's vendor. So get rid of it and include the missing dependency on device side device_framework_matrix.xml
    
    Signed-off-by: Kanishk <kanishkthederp@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -19,7 +19,6 @@ TARGET_NEEDS_DTBOIMAGE := true
 
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/crosshatch/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/voltage/config/device_framework_matrix.xml
 
 # Partitions
 AB_OTA_PARTITIONS += \
