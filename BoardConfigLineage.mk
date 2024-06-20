820300b07b84347b93bdd8881be9d58529dc719e
Michael Bestas <mkbestas@lineageos.org>
2024-06-20 13:22:57 +0300

    b1c1: Decrease /product reserved size again
    QPR3 and Gallery2 editor made the images go over the limit again.
    
    Change-Id: I9971a4e634c879d9cd019e68626265361d33e582
    Signed-off-by: Kanishk <kanishkthederp@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -28,9 +28,11 @@ ifneq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
 endif
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
-
-# Reserve some space for GApps
-include device/google/crosshatch/configs/reserved_space.mk
+# Reserve space for gapps install
+-include vendor/voltage/config/BoardConfigReservedSize.mk
+ifneq ($(WITH_GMS),true)
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1069547520
+endif
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy-lineage/dynamic
