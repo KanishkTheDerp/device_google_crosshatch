98c7eefa70ee353e7d80b5a6a51fece45e45fcdf
Kanishk <kanishkthederp@gmail.com>
2024-07-30 10:40:31 +0530

    b1c1: force reserved space
    Signed-off-by: Kanishk <kanishkthederp@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -29,7 +29,7 @@ endif
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Reserve space for gapps install
--include vendor/voltage/config/BoardConfigReservedSize.mk
+include vendor/voltage/config/BoardConfigReservedSize.mk
 ifneq ($(WITH_GMS),true)
 BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1069547520
 endif
